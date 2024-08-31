caa8d201732bcbddc0ce2c592fefd8c5c0b4c972
kc <kc@kc.com>
2024-08-31 16:56:58 +0800

    beatmapgen init

---
+++ beatmapgen_finetune.sh
@@ -0,0 +1,6 @@
+dora --verbose run  \
+    solver=musicgen/beatmapgen_base_32khz_test \
+    model/lm/model_scale=small \
+    continue_from=/mnt/workspace/audiocraft_download/beatmapgen_finetune_musicgen-small.th \
+    conditioner=none \
+    --clear
