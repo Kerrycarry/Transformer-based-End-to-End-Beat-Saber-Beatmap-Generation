2606003c4dfe46da546bdc186006a0932fe9c2df
kc <kc@kc.com>
2024-09-06 07:40:44 +0800

    migration change

--- finetune_stereo_musicgen-small.sh
+++ finetune_stereo_musicgen-small.sh
@@ -1,7 +1,7 @@
 dora --verbose run  \
     solver=musicgen/musicgen_base_32khz_test \
     model/lm/model_scale=small \
-    continue_from=/mnt/workspace/audiocraft_download/stereo_finetune_musicgen-small.th \
+    continue_from=/root/workspace/audiocraft_download/stereo_finetune_musicgen-small.th \
     conditioner=text2music \
     channels=2 interleave_stereo_codebooks.use=True \
     transformer_lm.n_q=8 transformer_lm.card=2048 \
